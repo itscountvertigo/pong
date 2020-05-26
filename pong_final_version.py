05f9e6e927161036db9caee9cb5a458ac726a51e
Anbiya Popal <55081188+itscountvertigo@users.noreply.github.com>
2020-05-26 12:29:28 +0200

    added comments and small change in on_key_press
    added comments: added more comments, second half of the code had practically no comments.
    
    on_key_press: switched 119/115 to arcade.key.W/arcade.key.D for the sake of consistency, I used that for most of my other code already. Also, I don't know all the number values so switching to up/down or any other keys is now easier, I can change it to arcade.key.UP instead of having to look up the correct number.

--- pong_final_version.py
+++ pong_final_version.py
@@ -42,6 +42,7 @@ class LeftPaddle(): # player, left paddle
         self.move_down = False
 
     def update(self):
+        # movement up and down. self.move_up and self.move_down are switched on/off in GameView's on_key_press/release
         if self.move_up == True:
             if self.y < SCREEN_HEIGHT - (self.height / 2):
                 self.y += PLAYER_SPEED
@@ -64,7 +65,7 @@ class RightPaddle(): # bot, right paddle
         self.color = OFF_WHITE
 
     def update(self, ball):
-        # the bot will only start moving if the ball is at bot_activation_x
+        # the bot will only start moving if the ball reaches bot_activation_x so the bot doesnt follow ball.x all the time
         if self.y > ball.y and ball.delta_x > 0 and ball.x > BOT_ACTIVATION_X:
             self.y -= BOT_SPEED
         
@@ -81,7 +82,7 @@ class RightPaddle(): # bot, right paddle
     def draw(self):
         arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.color)
 
-        # draws red guideline on the bot_activation_x, showing from which point the paddle will start moving
+        # draws red guideline on the bot_activation_x, showing from which ball.x the paddle will start moving
         # arcade.draw_rectangle_filled(BOT_ACTIVATION_X, SCREEN_HEIGHT/2, 2, SCREEN_HEIGHT, arcade.color.RED)
 
 class Ball():
@@ -106,44 +107,47 @@ class Ball():
         if self.y >= SCREEN_HEIGHT - self.size or self.y - self.size <= 0:
             self.delta_y = -self.delta_y
 
-        # respawning if ball hits left/right walls
+        # respawning if ball hits right wall
         if self.x >= SCREEN_WIDTH:
             self.x = SCREEN_WIDTH / 2
             self.y = SCREEN_HEIGHT / 2
             self.delta_x = -self.delta_x
             self.score_left += 1
 
-            # experiment: random respawn direction to make it a little more difficult
+            # experiment: random respawn direction (y-axis only) to make it a little more difficult
             if random.choice([True, False]) == True:
                 self.delta_y = -self.delta_y
-
+        
+        #respawning if ball hits left wall. left/right respawns are seperate becase of self.score_left/right.
         if self.x <= 0:
             self.x = SCREEN_WIDTH / 2
             self.y = SCREEN_HEIGHT / 2
             self.delta_x = -self.delta_x
             self.score_right += 1
 
-            # random respawn direction
+            # random respawn direction (y-axis)
             if random.choice([True, False]) == True:
                 self.delta_y = -self.delta_y
 
         self.x += self.delta_x
         self.y += self.delta_y
 
-        # collisions with paddles
+        # collision with paddles
         if self.x <= (leftpaddle.x + (leftpaddle.width / 2) + self.size) and self.y >= leftpaddle.y - (leftpaddle.height / 2)  and self.y <= leftpaddle.y + (leftpaddle.height / 2):
+            # while loop increases x by one if the ball clips into the paddle until it leaves. 
+            # this works because the if statement it's in checks the y value (this wont activate outside the paddle)
             while self.x <= (leftpaddle.x + (leftpaddle.width / 2) + self.size):
                 self.x += 1
 
             self.delta_x = -self.delta_x
 
         if self.x >= (rightpaddle.x - (rightpaddle.width / 2) - self.size) and self.y >= rightpaddle.y - (rightpaddle.height / 2) and self.y <= rightpaddle.y + (rightpaddle.height /2):
+            
             while self.x >= (rightpaddle.x - (rightpaddle.width / 2) - self.size):
                 self.x -= 1
             
             self.delta_x = -self.delta_x
             
-
     def draw(self):
         arcade.draw_circle_filled(self.x, self.y, self.size, self.color, self.num_segments)
 
@@ -151,7 +155,7 @@ class Scoreboard():
     def __init__(self):
            pass
 
-    def draw(self, ball):
+    def draw(self, ball): #takes ball as an argument for ball.score_left/right. 
         # score left
         arcade.draw_text(str(ball.score_left), (SCREEN_WIDTH / 2) - 100, SCREEN_HEIGHT - 75, OFF_WHITE, font_size=40, anchor_x="center", font_name=FONT)
 
@@ -178,10 +182,11 @@ class GameView(arcade.View): # view for the game
     def __init__(self):
         super().__init__()
         arcade.set_background_color(OFF_BLACK)
-
+        
+        # create the objects in the game
         self.ball = Ball()
         self.leftpaddle = LeftPaddle()
-        self.rightpaddle = RightPaddle()
+        self.rightpaddle = RightPaddle() # rightpaddle has its own class for the bot's movement
 
         self.scoreboard = Scoreboard()
 
@@ -189,11 +194,12 @@ class GameView(arcade.View): # view for the game
         arcade.start_render()
         arcade.set_background_color(OFF_BLACK)
 
+        # draw objects in the game
         self.ball.draw()
         self.leftpaddle.draw()
         self.rightpaddle.draw()
 
-        self.scoreboard.draw(self.ball)
+        self.scoreboard.draw(self.ball) # scoreboard takes ball as an argument for self.score_left/right
 
     def on_update(self, delta_time):
         self.leftpaddle.update()
@@ -202,10 +208,10 @@ class GameView(arcade.View): # view for the game
         self.ball.update(self.leftpaddle, self.rightpaddle)
 
     def on_key_press(self, key, key_modifiers):
-        if key == 119:
+        if key == arcade.key.W:
             self.leftpaddle.move_up = True
 
-        if key == 115:
+        if key == arcade.key.D:
             self.leftpaddle.move_down = True
 
         if key == arcade.key.ESCAPE:
@@ -213,7 +219,7 @@ class GameView(arcade.View): # view for the game
             self.window.show_view(pause)
 
     def on_key_release(self, key, key_modifiers):
-        if key == 119 or 115:
+        if key == arcade.key.W or arcade.key.D: # stops moving when keys are released
             self.leftpaddle.move_up = False
             self.leftpaddle.move_down = False
 
@@ -226,11 +232,11 @@ class PauseView(arcade.View): # view for pause screen. if esc is pressed, this v
         arcade.start_render()
         arcade.set_background_color(GRAY)
 
-        self.game_view.ball.draw()
+        self.game_view.ball.draw() 
          
         self.game_view.leftpaddle.draw() # render paused leftpaddle
         self.game_view.rightpaddle.draw() # render paused rightpaddle
-        self.game_view.scoreboard.draw(self.game_view.ball)
+        self.game_view.scoreboard.draw(self.game_view.ball) #renders paused scoreboard
 
         arcade.draw_text("PAUSED", SCREEN_WIDTH/2, SCREEN_HEIGHT/2, OFF_WHITE, font_size=50, anchor_x="center", font_name=FONT)
         arcade.draw_text("Press Esc. to return", SCREEN_WIDTH/2, SCREEN_HEIGHT/2-30, OFF_WHITE, font_size=20, anchor_x="center", font_name=FONT)
@@ -238,10 +244,10 @@ class PauseView(arcade.View): # view for pause screen. if esc is pressed, this v
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.ESCAPE:
-            self.window.show_view(self.game_view)
+            self.window.show_view(self.game_view) # if esc is pressed, return to the game_view in the state it was left
         elif key == arcade.key.ENTER:
             game = GameView()
-            self.window.show_view(game)
+            self.window.show_view(game) # if enter is pressed, return a new game_view
 
 def main():
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
